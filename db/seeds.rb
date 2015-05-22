b3d39e847082655f12b3334a0caaac374065c844
Arin Houck <arinhouck@gmail.com>
2015-05-21 21:49:34 -0700

    rest of devise changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if !User.any?
+  User.create(name: 'Arin', email: 'arinhouck@gmail.com', password: 'password')
+end
