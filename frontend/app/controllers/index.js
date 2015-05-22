872009599b359845d49c158eb2def91a882decb5
Arin Houck <arinhouck@gmail.com>
2015-05-21 19:28:05 -0700

    added modal

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  openModal: false,
+    actions: {
+      openModal: function(){
+        this.set('openModal', true)
+      }
+    }
+
 });
