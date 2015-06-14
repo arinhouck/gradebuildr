3b6ec1edd715ec952685a6f0a769b0b56a0c261d
Arin Houck <arinhouck@gmail.com>
2015-06-14 09:06:41 -0700

    create new course with grades finished

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,2 +1,33 @@
 class WeightsController < ApplicationController
+
+  def show
+    @weight = Weight.find(params[:id])
+    render json: @weight
+  end
+
+  def update
+    @weight = Weight.find(params[:id])
+    if @weight.update_attributes(params[:weight])
+      render json: @weight
+    else
+      render json: @weight.errors, status: 500
+    end
+  end
+
+  def create
+    @weight = Weight.new(weight_params)
+
+    if @weight.save
+      render json: @weight, status: :created
+    else
+      render json: @weight.errors, status: 500
+    end
+  end
+
+  private
+
+  def weight_params
+    params.require(:weight).permit(:name, :percentage, :course_id, :user_id)
+  end
+
 end
