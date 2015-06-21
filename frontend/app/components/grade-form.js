b7c926f762a1bd2869c5c1f35ed263fe6404d881
Arin Houck <arinhouck@gmail.com>
2015-06-21 10:18:54 -0700

    refactor grade-row, support empty records, dashboard grades

--- frontend/app/components/grade-form.js
+++ frontend/app/components/grade-form.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['form-group'],
 
   setWeight: function() {
     var grade = this.get('grade');
--- frontend/app/controllers/dashboard/index.js
+++ frontend/app/controllers/dashboard/index.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Controller.extend({
-  model: {}
+  model: {},
+
+  page: 1,
+  perPage: 5,
+
+  // can be called anything, I've called it pagedContent
+  // remember to iterate over pagedContent in your template
+  pagedGradeContent: pagedArray('model.grades', {pageBinding: "page", perPageBinding: "perPage"}),
+
 });
