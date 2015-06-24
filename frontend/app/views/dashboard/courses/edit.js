a2908262222bcd066577c9ec000393aa1474bbdf
Arin Houck <arinhouck@gmail.com>
2015-06-23 23:44:54 -0700

    rollback on weights edit view

--- frontend/app/views/dashboard/courses/edit.js
+++ frontend/app/views/dashboard/courses/edit.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.View.extend({
   didInsertElement: function() {
     this.set('controller.openModal', true);
+  },
+
+  willDestroyElement: function() {
+    this.get('controller.weights').filterProperty('isDirty').forEach(function(weight) {
+      weight.rollback();
+    });
   }
 
 });
