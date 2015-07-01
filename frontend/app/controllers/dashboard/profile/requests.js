5b261f9e71a75c9615d097e198df273b911395eb
Arin Houck <arinhouck@gmail.com>
2015-06-30 22:48:14 -0700

    delete requests

--- frontend/app/controllers/dashboard/profile/requests.js
+++ frontend/app/controllers/dashboard/profile/requests.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    deleteRequest: function(request) {
+      request.destroyRecord().then(function() {
+        $.growl.notice({title: 'Request', message: 'Successfully deleted.'});
+      });
+    }
+  }
 });
