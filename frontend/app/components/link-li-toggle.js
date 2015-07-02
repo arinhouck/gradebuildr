09546cf44504e232f3af7cb37ab6df7934f55602
Arin Houck <arinhouck@gmail.com>
2015-07-02 00:33:46 -0700

    fix toggle profile

--- frontend/app/components/link-li-toggle.js
+++ frontend/app/components/link-li-toggle.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Em.Component.extend({
     tagName: 'li',
     classNameBindings: ['active'],
-
+    click: function() {
+      $('body').toggleClass('sidebar-open');
+    },
     active: function() {
         return this.get('childViews').anyBy('active');
     }.property('childViews.@each.active')
