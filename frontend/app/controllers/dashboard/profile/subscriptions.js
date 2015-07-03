b73ef54a4c7a1da3bde8fbaf01fc35d10e1701e7
Arin Houck <arinhouck@gmail.com>
2015-07-03 13:07:18 -0700

    plan prices

--- frontend/app/controllers/dashboard/profile/subscriptions.js
+++ frontend/app/controllers/dashboard/profile/subscriptions.js
@@ -4,8 +4,8 @@ export default Ember.Controller.extend({
   name: 'Gradebuildr',
   logo: '/assets/apple-touch-icon-152x152.png',
   basic: {name: 'Basic', to_cents: 500},
-  premium: {name: 'Premium', to_cents: 1500},
-  ultimate: {name: 'Ultimate', to_cents: 5000},
+  premium: {name: 'Premium', to_cents: 2500},
+  ultimate: {name: 'Ultimate', to_cents: 10000},
   noPlan: Ember.computed.not('plan'),
   plan: null,
   actions: {
