b5e89a0e876004f58d10ec0d391a7c8680219eae
Arin Houck <arinhouck@gmail.com>
2015-08-05 22:06:20 -0700

    spinner for students and colspan changes

--- frontend/app/routes/dashboard/students/index.js
+++ frontend/app/routes/dashboard/students/index.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   setupController: function(controller, model) {
+    controller.set('isLoading', true);
     this.get('session.currentUser').then(function(user) {
        user.get('students').then(function(students) {
          controller.set('students', students);
+         controller.set('isLoading', false);
        });
     });
   }
