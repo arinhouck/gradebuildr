1768a2c23d8603fe74155df6818c7822f917f6d5
Arin Houck <arinhouck@gmail.com>
2015-08-06 22:46:36 -0700

    letter grade

--- frontend/app/models/course.js
+++ frontend/app/models/course.js
@@ -37,7 +37,6 @@ export default DS.Model.extend({
         weightGrades[i] = (gradePoints[i] / gradePointsTotal[i]) * weight.get('percentage');
       }
     })
-
     if (weightGrades.length == 0) {
       return 100;
     } else {
@@ -48,5 +47,93 @@ export default DS.Model.extend({
     }
 
   }.property('grades.@each.score', 'grades.@each.scoreTotal', 'grades.@each.weight', 'weights.@each.percentage', 'semester'),
+
+  scoreToGradePoints: function() {
+    var score = this.get('currentGrade');
+    var gradingScale = this.get('gradingScale');
+    var plus = gradingScale.indexOf('Plus') >= 0;
+    var minus = gradingScale.indexOf('Minus') >= 0;
+    debugger;
+    if (score >= 97) { // A+
+      if (plus) {
+        this.set('letterGrade', 'A+');
+        this.set('score', 4.333);
+      } else {
+        this.set('letterGrade', 'A');
+        this.set('score', 4);
+      }
+    } else if (score >= 93) { // A
+      this.set('letterGrade', 'A');
+      this.set('score', 4);
+    } else if (score >= 90) { // A-
+      if (minus) {
+        this.set('letterGrade', 'A-');
+        this.set('score', 3.667);
+      } else {
+        this.set('letterGrade', 'A');
+        this.set('score', 4);
+      }
+    } else if (score >= 87) { // B+
+      if (plus) {
+        this.set('letterGrade', 'B+');
+        this.set('score', 3.333);
+      } else {
+        this.set('letterGrade', 'B');
+        this.set('score', 3);
+      }
+    } else if (score >= 83) { // B
+      this.set('letterGrade', 'B');
+      this.set('score', 3);
+    } else if (score >= 80) { // B-
+      if (minus) {
+        this.set('letterGrade', 'B-');
+        this.set('score', 2.667);
+      } else {
+        this.set('letterGrade', 'B');
+        this.set('score', 3);
+      }
+    } else if (score >= 77) { // C+
+      if (plus) {
+        this.set('letterGrade', 'C+');
+        this.set('score', 2.333);
+      } else {
+        this.set('letterGrade', 'C');
+        this.set('score', 2);
+      }
+    } else if (score >= 73) { // C
+      this.set('letterGrade', 'C');
+      this.set('score', 2);
+    } else if (score >= 70) { // C-
+      if (minus) {
+        this.set('letterGrade', 'C-');
+        this.set('score', 1.667);
+      } else {
+        this.set('letterGrade', 'C');
+        this.set('score', 2);
+      }
+    } else if (score >= 67) { // D+
+      if (plus) {
+        this.set('letterGrade', 'D+');
+        this.set('score', 1.333);
+      } else {
+        this.set('letterGrade', 'D');
+        this.set('score', 1);
+      }
+    } else if (score >= 63) { // D
+      this.set('letterGrade', 'D');
+      this.set('score', 1);
+    } else if (score >= 60) { // D-
+      if(minus) {
+        this.set('letterGrade', 'D-');
+        this.set('score', 0.667);
+      } else {
+        this.set('letterGrade', 'D');
+        this.set('score', 1);
+      }
+    } else {
+      this.set('letterGrade', 'F');
+      this.set('score', 0);
+    }
+  }.observes('currentGrade', 'creditHours', 'user.activeSemester')
 
 });
--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -25,7 +25,7 @@ export default DS.Model.extend({
     var gradePoints = 0;
     var self = this;
     courses.forEach(function(course){
-      gradePoints += self.scoreToGradePoints(course.get('currentGrade'), course)*(course.get('creditHours'))
+      gradePoints += course.get('score')*(course.get('creditHours'))
     });
     return gradePoints;
   }.property('courses.@each.currentGrade', 'courses.@each.creditHours', 'activeSemester'),
@@ -82,62 +82,6 @@ export default DS.Model.extend({
 
     var cumulativeGpa = totalPoints / totalUnits;
     return cumulativeGpa.toFixed(2);
-  }.property('semesterGradePoints', 'semesterCreditHours', 'gradePoints', 'gradeUnits', 'activeSemester'),
-
-  scoreToGradePoints: function(score, course) {
-    var gradingScale = course.get('gradingScale');
-    var plus = gradingScale.indexOf('Plus') >= 0;
-    var minus = gradingScale.indexOf('Minus') >= 0;
-    if (score >= 97) // A+
-      if (plus)
-        return 4.333;
-      else
-        return 4;
-    else if (score >= 93) // A
-      return 4;
-    else if (score >= 90) // A-
-      if (minus)
-        return 3.667;
-      else
-        return 4;
-    else if (score >= 87) // B+
-      if (plus)
-        return 3.333;
-      else
-        return 3;
-    else if (score >= 83) // B
-      return 3;
-    else if (score >= 80) // B-
-      if (minus)
-        return 2.667;
-      else
-        return 3;
-    else if (score >= 77) // C+
-      if (plus)
-        return 2.333;
-      else
-        return 2;
-    else if (score >= 73) // C
-      return 2;
-    else if (score >= 70) // C-
-      if (minus)
-        return 1.667;
-      else
-        return 2;
-    else if (score >= 67) // D+
-      if (plus)
-        return 1.333;
-      else
-        return 1;
-    else if (score >= 63) // D
-      return 1;
-    else if (score >= 60) // D-
-      if(minus)
-        return 0.667;
-      else
-        return 1;
-    else
-      return 0;
-  }
+  }.property('semesterGradePoints', 'semesterCreditHours', 'gradePoints', 'gradeUnits', 'activeSemester')
 
 });
