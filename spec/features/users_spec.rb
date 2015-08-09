15482ff0436ece4b6539b2eee4ccc49f57a29634
Arin Houck <arinhouck@gmail.com>
2015-08-09 11:16:49 -0700

    refactor test a bit

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,13 +5,15 @@ describe "Users", type: :feature, :js => true do
     ['Spring 2015', 'Summer 2015', 'Fall 2015'].each do |name|
       Semester.create(name: name)
     end
+    user = User.create(name: 'John Smith', email: 'john.smith@example.com', password: 'password', active_semester: 'Spring 2015')
+    user.confirm
   end
 
   it "can register" do
     visit '/'
     click_link 'register'
     fill_in 'name', with: 'John Doe'
-    fill_in 'email', with: 'john@example.com'
+    fill_in 'email', with: 'john.doe@example.com'
     select 'Fall 2015', :from => "activeSemester"
     fill_in 'password', with: 'password'
     fill_in 'passwordConfirmation', with: 'password'
@@ -23,11 +25,9 @@ describe "Users", type: :feature, :js => true do
   end
 
   it "can login" do
-    user = User.create(name: 'John doe', email: 'john@example.com', password: 'password', active_semester: 'Spring 2015')
-    user.confirm!
     visit '/'
     click_link 'login-nav'
-    fill_in 'email', with: 'john@example.com'
+    fill_in 'email', with: 'john.smith@example.com'
     fill_in 'password', with: 'password'
     click_button 'Log in'
     wait_for_ajax
