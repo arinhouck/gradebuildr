0fa17a74d7d73abdc6ee816e820d77e71c32d12f
Arin Houck <arinhouck@gmail.com>
2015-08-13 19:33:50 -0700

    student support, first/last name, remove requests

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :email, :name, :grade_points, :grade_units, :active_semester,
-   :student_ids, :director_ids, :is_director, :active_until, :subscription, :canceled_subscription,
-   :unconfirmed_email
-  has_many :requests
-  has_many :received_requests
+  attributes :id, :email, :first_name, :last_name, :organization, :account_type,
+   :is_student, :is_organization, :grade_points, :grade_units, :active_semester,
+   :active_until, :subscription, :canceled_subscription, :unconfirmed_email
+  has_many :students, :serializer => StudentSerializer
   has_many :courses
   has_many :grades
 end
