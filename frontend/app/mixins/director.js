e6c1c74640837c7068fd5a38c178f1d3d8e6c9ef
Arin Houck <arinhouck@gmail.com>
2015-08-13 19:35:29 -0700

    check on isOrganization now

--- frontend/app/mixins/director.js
+++ frontend/app/mixins/director.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   beforeModel: function(transition) {
-    if (!this.get('session.currentUser.isDirector')) {
+    if (!this.get('session.currentUser.isOrganization')) {
       transition.abort();
       this.transitionTo('dashboard');
     }
