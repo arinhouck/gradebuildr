e85b99162c7a4cb9dc4aab6335d11092bc3d4764
Arin Houck <arinhouck@gmail.com>
2015-08-18 19:47:43 -0700

    add columns to model

--- frontend/app/models/director.js
+++ frontend/app/models/director.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  organization: DS.attr('string')
+  organization: DS.attr('string'),
+  groupCode: DS.attr('string'),
+  email: DS.attr('string')
 });
