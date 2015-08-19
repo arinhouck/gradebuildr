3ff3f1da4d33ce3fc14f86674fc777df720a984c
Arin Houck <arinhouck@gmail.com>
2015-08-19 16:40:05 -0700

    dashboard pagination and load courses/grades

--- frontend/app/routes/dashboard/index.js
+++ frontend/app/routes/dashboard/index.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
+  perPage: 5,
+
   model() {
     return this.get('session.currentUser');
   },
   afterModel(model) {
-    return Ember.RSVP.hash({
-      grades: model.get('grades'),
-      courses: model.get('courses'),
-      weights: model.get('weights')
-    });
+    var params = this.paramsFor();
+    params.user_id = model.get('id');
+    this.controllerFor('dashboard.index').set('grades', this.findPaged('grade', params));
+    this.controllerFor('dashboard.index').set('courses', this.findPaged('course', params));
   }
 });
