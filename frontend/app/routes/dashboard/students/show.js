a1b10fe119438f41d9d676456a66c42661ef0050
Arin Houck <arinhouck@gmail.com>
2015-08-19 16:39:53 -0700

    load student by id and set loading

--- frontend/app/routes/dashboard/students/show.js
+++ frontend/app/routes/dashboard/students/show.js
@@ -3,8 +3,9 @@ import Ember from 'ember';
 import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({
-  model() {
-    return this.get('model');
+  model(params) {
+    this.controllerFor('dashboard.students').set('isLoading', true);
+    return this.store.find('student', params.user_id);
   },
   afterModel(model) {
     return Ember.RSVP.hash({
