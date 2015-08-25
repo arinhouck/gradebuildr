e982f1d0f089b91def17031c1e396d258b2d55f5
Arin Houck <arinhouck@gmail.com>
2015-08-24 23:05:18 -0700

    force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session, if: :json_request?
   skip_before_action :verify_authenticity_token, if: :json_request?
+  force_ssl :if => :needs_ssl_redirect?
 
 
   protected
+
+  def needs_ssl_redirect?
+    # Due to Cloudflare’s configuration,
+    # the HTTPS ENV var is not set.
+    # We’ll check for their custom header instead:
+    !request.headers["HTTP_CF_VISITOR"].to_s.include?("https") && !%w(development test).include?(Rails.env)
+  end
 
   def authenticate_user_from_token!
     authenticated = authenticate_with_http_token do |user_token, options|
