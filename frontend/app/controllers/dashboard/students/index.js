8e7b195b576cfd8de6a45db032617610b178baf9
Arin Houck <arinhouck@gmail.com>
2015-09-26 10:31:02 -0700

    sorted setup for students controller

--- frontend/app/controllers/dashboard/students/index.js
+++ frontend/app/controllers/dashboard/students/index.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  students: Ember.computed.map('model.students', item => item)
+  students: Ember.computed.map('model.students', item => item),
+  sortProperties: ['firstName'], // Default sort
+  sortAscending: true,
+
+  sortedStudents: function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: this.get('sortProperties'),
+      sortAscending: this.get('sortAscending'),
+      content: this.get('model.students')
+    });
+  }.property('students'),
+
 });
